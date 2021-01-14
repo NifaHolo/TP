d17371432a37c04072290e06aabb55ac620ee1e0
julien <julien.leconte2002@gmail.com>
2021-01-14 16:05:38 +0100

    tp noté pokedex

--- TP_Poke/pokedex_etu/index.php
+++ TP_Poke/pokedex_etu/index.php
@@ -5,24 +5,48 @@
     <link rel="stylesheet" href="style.css">
     <title>Pokedex</title>
   </head>
-  <body>
+ <body>
  <h1>My Pokedex</h1>
     <table>
       <thead>
-          <?php
-$link = mysqli_connect("localhost", "root" , "" , "pokedex");
+        <?php $link = mysqli_connect("localhost", "root" , "" , "pokedex");
     
     if(!$link){
     exit;
    }
 $req=  'SELECT id,identifier,height,weight,base_experience FROM pokemon ;'; 
-$result = mysqli_query($link,$req);
+$num=  'SELECT id FROM pokemon where identifier = "mew" ;';
 
+$result = mysqli_query($link,$req);
+$result2 = mysqli_query($link,$num);
 if ($result) 
 {
+  if($raw = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
+    echo"There is". $raw["id"] ." pokemons from the satabase ". "<br>";
+}
+  ?>
+        <tr>
+          <strong>
+          <th>Sprite</th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Height</th>
+          <th>Weight</th>
+          <th>Base exp</th>
+        </strong>
+        </tr>
+      </thead>
+    <table>
+       
+      <thead>
+       
+          <?php
+
     while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
     {
+      
            echo"<tr>";
+
                  if($row["base_experience"]>200){
                echo"<td><img src='sprites/" . $row["identifier"] . ".png' alt=". $row["identifier"] . "></td>";
                 echo "<td class='super'>" . $row["id"] . "</td>";
